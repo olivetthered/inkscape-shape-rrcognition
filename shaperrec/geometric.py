2da97b6862e18cc6d9942092543013d053380bb9
olivetthered <37796246+olivetthered@users.noreply.github.com>
2020-11-15 11:31:22 +0000

    tidied up bezier rendering, all non relative beziers are now support so only relative and arc left to support then all curved segment types of a PATH will been been supported. This is starting to highlight bugs in the code because the last point of the PATH is being removed somewhere.

--- shaperrec/geometric.py
+++ shaperrec/geometric.py
@@ -17,38 +17,52 @@ curveFragments = 10
 
 def qudSmRelBezCurFrag(ctrlPts, startPoint):
     #just call the normal one with adjusted coordinates
+    ctrlPts[0] = startPoint[-2] + ctrlPts[0]
+    ctrlPts[1] =  startPoint[-1] + ctrlPts[1]
     return qudSmBezCurFrag(ctrlPts, startPoint)
     
 def qudSmBezCurFrag(ctrlPts, startPoint):
     #There are no control points
     debug("startPoint: '", startPoint, "'")
     debug("shouldbethehandle: '", [startPoint[2] + (startPoint[2] - startPoint[0]), startPoint[3] + (startPoint[3] - startPoint[1])], "'")
-    ctrlPtsExt = [startPoint[2] + (startPoint[2] - startPoint[0]), startPoint[3] + (startPoint[3] - startPoint[1])] + ctrlPts
+    #ctrlPtsExt = [startPoint[2] + (startPoint[2] - startPoint[0]), startPoint[3] + (startPoint[3] - startPoint[1])] + ctrlPts
+    ctrlPtsExt = [startPoint[-2] + (startPoint[-2] - startPoint[-4]), startPoint[-1] + (startPoint[-1] - startPoint[-3])] + ctrlPts
+    debug("startPoint: '", startPoint, "'") 
+    debug("ctrlPts: '", ctrlPts, "'")     
+    debug("startPoint[-2:]: '", startPoint[-2:], "'") 
+    debug("ctrlPtsExt: '", ctrlPtsExt, "'") 
     debug("shound be the same as non smooth: '", ctrlPtsExt)
-    return cubBezCurFrag(ctrlPtsExt, startPoint[2:4])
+    return cubBezCurFrag(ctrlPtsExt, startPoint[-2:])
     
 def qudRelBezCurFrag(ctrlPts, startPoint):
     #just call the normal one with adjusted coordinates
+    ctrlPts[0] = startPoint[-2] + ctrlPts[0]
+    ctrlPts[1] =  startPoint[-1] + ctrlPts[1]
     return qudBezCurFrag(ctrlPts, startPoint)
     
 def qudBezCurFrag(ctrlPts, startPoint):
-    #Fixme: Write the code to render a quad.
-    return cubBezCurFrag(ctrlPts, startPoint)
+    #tested working
+    debug("startPoint: '", startPoint, "'") 
+    return cubBezCurFrag(ctrlPts, startPoint[-2:])
 
 def cubSmRelBezCurFrag(ctrlPts, startPoint):
     #just call the normal one with adjusted coordinates
+    #[prevL[-1][0] + x[0:1][0] , prevL[1] + x[1:2][1]]
+    ctrlPts[0] = startPoint[-2] + ctrlPts[0]
+    ctrlPts[1] =  startPoint[-1] + ctrlPts[1]
     return cubSmBezCurFrag(ctrlPts, startPoint)
     
 def cubSmBezCurFrag(ctrlPts, startPoint):
-    #just call the normal one with adjusted coordinates
-    ctrlPtsExt = [(startPoint[0] + ctrlPts[0] - ctrlPts[2] ), (startPoint[1] + ctrlPts[1] - ctrlPts[3] )] + ctrlPts
-    return cubBezCurFrag(ctrlPtsExt, startPoint)
+    #tested working
+    #just call the normal one with adjusted coordinates    
+    ctrlPtsExt = [startPoint[-2] + (startPoint[-2] - startPoint[-4]), startPoint[-1] + (startPoint[-1] - startPoint[-3])] + ctrlPts
+    return cubBezCurFrag(ctrlPtsExt, startPoint[-2:])
 
 def cubRelBezCurFrag(ctrlPts, startPoint):
     #just call the normal one with adjusted coordinates
+    ctrlPts[0] = startPoint[-2] + ctrlPts[0]
+    ctrlPts[1] =  startPoint[-1] + ctrlPts[1]
     return cubBezCurFrag(ctrlPts, startPoint)
-
-
 
 def compute(t, points): #, _3d
     #// shortcuts
@@ -83,10 +97,14 @@ def compute(t, points): #, _3d
         return ret
 
     #// quadratic/cubic curve?
+    mt2 = 0
+    a = b = c = d = 0
     if (order < 4):
         mt2 = mt * mt
         t2 = t * t
-        a = b = c = d = 0
+    else:
+        sys.stderr.write("Order :'" + str(order) +"' beyond limits of function")
+        return [0.0, 0.0]
 
     if (order == 2):
         p = p + [0,0]
@@ -108,9 +126,10 @@ def compute(t, points): #, _3d
     #      }
     return ret
     
-
+#TODO: Number of fragments should possibly be based upon the length of the segment.
 
 def cubBezCurFrag(ctrlPts, startPoint):
+    #tested working
      points =[]
      rng = range(0, curveFragments + 1)
      #sx = startPoint[0]
@@ -118,7 +137,7 @@ def cubBezCurFrag(ctrlPts, startPoint):
      debug("control ", startPoint + ctrlPts)
      inputPoints = startPoint + ctrlPts
      for i in rng:
-         t = (float(i + 1) / float(len(rng)))
+         t = (float(i) / float(len(rng) - 1))
          debug("t ", t,  ":", i)
          newp = compute(t, inputPoints)
          #newp= [(1-t) ** 3 * startPoint[0] + 3 * ((1-t) ** 2) * t* ctrlPts[0] +3 * (1-t) * (t ** 2) * ctrlPts[2] + (t ** 3) * ctrlPts[4],
@@ -138,39 +157,43 @@ def toArray(parsedList):
     # The set of commands is now complete, all absolute positioning has been tested, relative positioning still neds some more testing.
     # Curved parts of the path need fragmenting instead of just being taken as a straight line.
     interpretCommand = {
-        'C': lambda x, prevL : cubBezCurFrag(x, prevL[-1]), # cubic bezier curve. Ignore the curve. #TODO, fragment
-        'c': lambda x, prevL : cubRelBezCurFrag(x, prevL[-1]), # cubic bezier curve, relative. Ignore the curve. #TODO, fragment        
-        'S': lambda x, prevL : cubSmBezCurFrag(x, prevL[-1]), # cubic bezier curve, smooth. TODO, fragment
-        's': lambda x, prevL : cubSmRelBezCurFrag(x, prevL[-1]), # cubic bezier curve, smooth, relative. TODO, fragment
-        'Q': lambda x, prevL : qudBezCurFrag(x, prevL[-1]), # quadratic bezier curve. TODO, fragment
-        'q': lambda x, prevL : qudRelBezCurFrag(x, prevL[-1]), # quadratic bezier curve, relative TODO, fragment        
-#[[prevL[-1][0] + x[0:1][0] , prevL[-1][1] + x[1:2][1]]], # quadratic bezier curve, relative TODO, fragment                
-        'T': lambda x, prevL : qudSmBezCurFrag(x, prevL[-1]), # quadratic bezier curve, smooth. TODO, fragment
-        't': lambda x, prevL : qudSmRelBezCurFrag(x, prevL[-1]), # quadratic bezier curve, smooth, relative. TODO, fragment
+        'C': lambda x, prevL : cubBezCurFrag(x, prevL[-2:]), # cubic bezier curve. Ignore the curve. #TODO, fragment
+        'c': lambda x, prevL : cubRelBezCurFrag(x, prevL[-2:]), # cubic bezier curve, relative. Ignore the curve. #TODO, fragment        
+        'S': lambda x, prevL : cubSmBezCurFrag(x, prevL), # cubic bezier curve, smooth. TODO, fragment
+        's': lambda x, prevL : cubSmRelBezCurFrag(x, prevL), # cubic bezier curve, smooth, relative. TODO, fragment
+        'Q': lambda x, prevL : qudBezCurFrag(x, prevL), # quadratic bezier curve. TODO, fragment
+        'q': lambda x, prevL : qudRelBezCurFrag(x, prevL), # quadratic bezier curve, relative TODO, fragment        
+#[[prevL[-1][0] + x[0:1][0] , prevL[1] + x[1:2][1]]], # quadratic bezier curve, relative TODO, fragment                
+        'T': lambda x, prevL : qudSmBezCurFrag(x, prevL), # quadratic bezier curve, smooth. TODO, fragment
+        't': lambda x, prevL : qudSmRelBezCurFrag(x, prevL), # quadratic bezier curve, smooth, relative. TODO, fragment
         'L': lambda x, prevL : [x[0:2]], # Line
-        'l': lambda x, prevL : [[prevL[-1][0] + x[0:1][0] , prevL[-1][1] + x[1:2][1]]], # Line, relative
+        'l': lambda x, prevL : [[prevL[0] + x[0:1][0] , prevL[1] + x[1:2][1]]], # Line, relative
         'M': lambda x, prevL : [x[0:2]], # Move
-        'm': lambda x, prevL : [[prevL[-1][0] + x[0:1][0] , prevL[-1][1] + x[1:2][1]]], # Move, Relative
-        'H': lambda x, prevL : [[x[0:1][0],prevL[-1][1]]], # Horizontal
-        'h': lambda x, prevL : [[prevL[-1][0] + x[0:1][0], prevL[-1][1]]], # Horizontal , relative
-        'V': lambda x, prevL : [[prevL[-1][0], x[0:1][0]]], # Verticle
-        'v': lambda x, prevL : [[prevL[-1][0], prevL[-1][1] + x[0:1][0]]], # Verticle, relative
+        'm': lambda x, prevL : [[prevL[0] + x[0:1][0] , prevL[1] + x[1:2][1]]], # Move, Relative
+        'H': lambda x, prevL : [[x[0:1][0],prevL[1]]], # Horizontal
+        'h': lambda x, prevL : [[prevL[0] + x[0:1][0], prevL[1]]], # Horizontal , relative
+        'V': lambda x, prevL : [[prevL[0], x[0:1][0]]], # Verticle
+        'v': lambda x, prevL : [[prevL[0], prevL[1] + x[0:1][0]]], # Verticle, relative
         'A': lambda x, prevL : [x[5:7]], # Arc segment
-        'a': lambda x, prevL : [[prevL[-1][0] + x[5:6][0] , prevL[-1][1] + x[6:7][1]]], # Arc segment, relative
+        'a': lambda x, prevL : [[prevL[0] + x[5:6][0] , prevL[1] + x[6:7][1]]], # Arc segment, relative
         'Z': lambda x, prevL : [prevL[0]], # Close path
         'z': lambda x, prevL : [prevL[0]], # Close path
         }
-    
-    parseLookBack = []
-    parseLookBack = collections.deque(parseLookBack)
+
+    #append the last set of attrubutes of the first element to the lookBack for cases where smooth or smooth quad segments appear at the begining of a path.
+    lookBack = parsedList[0][1] + parsedList[0][1]
     points =[]
     for i, (c, arg) in enumerate(parsedList):
         debug('toArray ', i, c , arg)
-        debug('parseLookBack: ', parseLookBack)
-        newp = interpretCommand[c](arg, parseLookBack)
-        parseLookBack.append(arg)
-        while len(parseLookBack) > 1:
-            parseLookBack.popleft()
+        debug('lookBack: ', lookBack)
+        newp = interpretCommand[c](arg, lookBack)
+        
+        #double up if there are only two point entries to support transition into smooth beziers or arrays of smooth beziers etc..
+        if len(arg) == 2:
+            arg = arg + arg
+
+        #we only need to keep the last element in the lookBack, so remove any elemenmts in front.
+        lookBack = arg
 
         debug('newPoints ', newp)
         points = points + newp
@@ -179,6 +202,7 @@ def toArray(parsedList):
     # Some times we have points *very* close to each other
     # these do not bring any meaning full info, so we remove them
     #
+    
     x, y, w, h = computeBox(a)
     sizeC = 0.5*(w+h)
     #deltas = numpy.zeros((len(a),2) )
@@ -186,7 +210,7 @@ def toArray(parsedList):
     #deltas[-1] = a[0] - a[-1]
     deltaD = numpy.sqrt(numpy.sum( deltas**2, 1 ))
     sortedDind = numpy.argsort(deltaD)
-    # expand longuest segments
+#    # expand longuest segments
     nexp = int(len(deltaD)*0.9)
     newpoints=[ None ]*len(a)
     medDelta = deltaD[sortedDind[int(len(deltaD)/2)] ]
@@ -202,13 +226,13 @@ def toArray(parsedList):
                 pL.append( (1-f)*a[ind]+f*a[ind+1] )
             newpoints[ind] = pL
         else:
-            newpoints[ind]=[a[ind]]
+             newpoints[ind]=[a[ind]]
     if(D(a[0], a[-1])/sizeC > 0.005 ) :
         newpoints[-1]=[a[-1]]
 
     points = numpy.concatenate([p for p in newpoints if p!=None] )
-    ## print ' medDelta ', medDelta, deltaD[sortedDind[-1]]
-    ## print len(a) ,' ------> ', len(points)
+#    ## print ' medDelta ', medDelta, deltaD[sortedDind[-1]]
+#    ## print len(a) ,' ------> ', len(points)
 
     rel_norms = numpy.sqrt(numpy.sum( deltas**2, 1 )) / sizeC
     keep = numpy.concatenate([numpy.where( rel_norms >0.005 )[0], numpy.array([len(a)-1])])
